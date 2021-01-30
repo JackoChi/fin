de8cd1cb5639a52eea8a400e048e8581c75fa673
Jacko <zchi.work@gmail.com>
2021-01-30 16:26:13 +0800

    add hist of daily returns. modify index as datetime

--- fin.py
+++ fin.py
@@ -6,8 +6,14 @@ ts.set_token('66d6adc50f850a0cfaf56f9ec023db9ec88179e8d1b314f08850a28d')
 pro = ts.pro_api()  
 
 #002475.SZ 立讯精密
-data = ts.pro_bar(ts_code='002475.SZ', adj='qfq', start_date='20190101', end_date='20200121')
-df = data[ 'close']
+df_temp = ts.pro_bar(ts_code='002475.SZ', adj='qfq', start_date='20190101', end_date='20200121')
+#dates = pd.date_range('20190101', '20200121')
+#df = pd.DataFrame(index=dates)
+df= df_temp[['trade_date', 'close']]
+#df = df.join(df_temp)
+#df['date'] = pd.to_datetime(df['trade_date'])
+#df.drop('trade_date', axis = 1, inplace = True)
+df.set_index('trade_date', inplace = True)
 
 def get_rolling_mean(values, window):
     return pd.roll_mean(values, window)
@@ -21,7 +27,7 @@ def get_bollinger_band(rm, rstd):
     return upper_band, lower_band 
 
 def get_daily_returns(df):
-    return ((df / df.shift(1, fill_value = 0)) - 1)
+    return ((df / df.shift(1)).fillna(0) - 1)
 
 def plot_data(df, title="Stock prices", xlabel="Date", ylabel="Price"):
     """Plot stock prices with a custom title and meaningful axis labels."""
@@ -30,6 +36,11 @@ def plot_data(df, title="Stock prices", xlabel="Date", ylabel="Price"):
     ax.set_ylabel(ylabel)
     plt.show()
 
+def plot_dailyreturns(df, bins = 20, title = "Hist of Daily returns"):
+    
+    df.hist(bins = bins)
+    plt.show()
+
 if __name__ == "__main__":
     daily_returns = get_daily_returns(df)
-    plot_data(daily_returns, title="Daily returns", ylabel="Daily returns")
+    plot_dailyreturns(daily_returns)
