5bb98d217de7d320eda94513d5c721ba38651bab
Jacko <zchi.work@gmail.com>
2021-02-03 09:04:33 +0800

    create a new dir for ML. add a skeleton for regression

---
+++ ml/linreglearner.py
@@ -0,0 +1,15 @@
+class LinRegLearner:
+    """
+    a skeleton for regression model
+
+    """
+    
+    def __init__(self):
+        pass
+
+    def train(X, y):
+        self.m, self.b = linear()
+
+    def query(X):
+        y = self.m * X + self.b 
+        return y 
