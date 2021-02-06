27fb722637dc6ca9da8ee34a25ea504ac5692943
Jacko <zchi.work@gmail.com>
2021-02-06 21:54:54 +0800

    add docstings and comments for def

--- fin.py
+++ fin.py
@@ -6,22 +6,37 @@ ts.set_token('66d6adc50f850a0cfaf56f9ec023db9ec88179e8d1b314f08850a28d')
 pro = ts.pro_api()  
 
 #002475.SZ 立讯精密
+#create an instance to request data from Tushare
 df_temp = ts.pro_bar(ts_code='002475.SZ', adj='qfq', start_date='20190101', end_date='20200121')
-#dates = pd.date_range('20190101', '20200121')
-#df = pd.DataFrame(index=dates)
 df= df_temp[['trade_date', 'close']]
-#df = df.join(df_temp)
-#df['date'] = pd.to_datetime(df['trade_date'])
-#df.drop('trade_date', axis = 1, inplace = True)
+#set timestamp as index
 df.set_index('trade_date', inplace = True)
 
 def get_rolling_mean(values, window):
+    """
+    get rolling mean
+
+    arg:
+    df and period
+    """
     return pd.roll_mean(values, window)
 
 def get_rolling_std(values, window):
+    """
+    get rolling standed variance
+
+    arg:
+    df and period
+    """
     return pd.rolling_std(values, window)
 
 def get_bollinger_band(rm, rstd):
+    """
+    get brollinger band 
+
+    arg:
+    rolling mean and rolling standed deviation 
+    """
     upper_band = rm + 2 * rstd
     lower_band = rm - 2 * rstd
     return upper_band, lower_band 
